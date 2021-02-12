3c9aa2a4f1a08714ad81291f8c6a92b278a67626
Blaire <grant.blaire@gmail.com>
2021-02-11 22:36:49 -0600

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,21 @@
 class Pirate
+
+    attr_accessor :name, :weight, :height
+    @@all = []
+
+    def initialize(params)
+        params.each do |k, v|
+            self.send("#{k}=", v) if self.respond_to?("#{k}=")
+        end
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,25 @@
 class Ship
+
+    attr_accessor :name, :type, :booty
+    @@all = []
+
+    def initialize(params)
+        params.each do |k, v|
+            self.send("#{k}=", v) if self.respond_to?("#{k}=")
+        end
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all.clear
+    end
+
 end
